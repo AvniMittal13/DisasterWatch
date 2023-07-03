0ffc2cae836722449fe1d788f09fb38d59709cfa
unknown <avni.mittal2002@gmail.com>
2023-07-03 22:29:43 +0530

    dummy commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from utils import forest_fire_prediction
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True)
 
 @app.route('/')
 def index():
